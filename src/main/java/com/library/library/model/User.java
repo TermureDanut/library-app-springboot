dd208f85eeecc529bf7a3d823f447f78a3e71acc
danut <danutvaleriu.termure@gmail.com>
2023-04-15 11:40:10 +0300

    Adds a new model named User that is an entity and a new dependency in the pom file for using entities.

---
+++ src/main/java/com/library/library/model/User.java
@@ -0,0 +1,33 @@
+package com.library.library.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column
+    private String name;
+    public User(){}
+    public User(String name){
+        this.name = name;
+    }
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+}
