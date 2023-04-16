aafe69d02018f3399bf04efb11a72871c97c699b
danut <danutvaleriu.termure@gmail.com>
2023-04-16 16:00:17 +0300

    Adds modifications on interface for one-to-many relationship construction.

--- src/main/java/com/library/library/repository/AuthorRepository.java
+++ src/main/java/com/library/library/repository/AuthorRepository.java
@@ -2,11 +2,10 @@ package com.library.library.repository;
 
 import com.library.library.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Optional<Author> findById(long id);
-    List<Author> findByFullName(String fullName);
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
