92dc0d9b8ccdfafee492f5bdadc7d79812b2b40e
danut <danutvaleriu.termure@gmail.com>
2023-04-16 15:21:20 +0300

    Modifies the AuthorRepository so it uses JPARepo not CRUDRepo.

--- src/main/java/com/library/library/repository/AuthorRepository.java
+++ src/main/java/com/library/library/repository/AuthorRepository.java
@@ -1,11 +1,15 @@
 package com.library.library.repository;
 
 import com.library.library.model.Author;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface AuthorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    List<Author> findByUserId(long id);
+    @Transactional
+    void deleteByUserId(long id);
 }
