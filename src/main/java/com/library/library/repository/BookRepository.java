9252f7506a658c43fa7cb52e318b74aed50528f2
danut <danutvaleriu.termure@gmail.com>
2023-05-01 16:40:11 +0300

    Added new method in BookRepository.

--- src/main/java/com/library/library/repository/BookRepository.java
+++ src/main/java/com/library/library/repository/BookRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findBookByAuthorsId(Long authorId);
+    List<Book> findBookByBookName(String bookName);
 }
